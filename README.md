289467d51bb6f6f029d1b5988ca2c6f55ff7352c
pretzelogical <yingyangyo266@gmail.com>
2024-03-26 17:15:13 -0500

    add SQL database

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # atlas-the-joy-of-painting-api
 
-This project uses the concepts of ETL (extract, transform, load) to take different unorganized but related data sources and transforming them to a more "normal" form that will be stored in some database.
+This project uses the concepts of ETL (extract, transform, load) to take different unorganized but related data sources and storing them in a SQL server.
+
+UML documentation is done with [draw.io](https://draw.io)
+
+# SQL server (./sql)
+
+SQL server runs as a docker container. To build and run use these commands inside the sql folder. The api server's user is set as read only since the data will not need to be modified afterwards.
+```
+make build
+make run
+```
---
+++ sql/init.sql
@@ -0,0 +1,33 @@
+-- Initialize joy of painting sql server
+
+-- Create database
+CREATE DATABASE `joy_of_painting`;
+USE `joy_of_painting`;
+
+-- Create tables
+CREATE TABLE `painting` (
+    `id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `painting_index` INT NOT NULL,
+    `img_src` VARCHAR(255) NOT NULL,
+    `youtube_src` VARCHAR(255) NOT NULL,
+    `colors` JSON NOT NULL,
+    `colors_hex` JSON NOT NULL,
+    `subject` JSON NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `episode` (
+    `id` INT NOT NULL,
+    `season` INT NOT NULL,
+    `episode` INT NOT NULL,
+    `air_date` DATE NOT NULL,
+    `painting_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`painting_id`) REFERENCES `painting` (`id`)
+);
+
+-- Create user and grant select (read-only access)
+CREATE USER jop_user@localhost IDENTIFIED BY 'bobross';
+GRANT SELECT ON joy_of_painting.* TO 'jop_user'@'localhost';
+FLUSH PRIVILEGES;
