37e3f68c30b38c861539133b785ae8a2557976f0
pretzelogical <yingyangyo266@gmail.com>
2024-03-28 15:04:19 -0500

    upload data to mySQL server

--- ETL/main.py
+++ ETL/main.py
@@ -2,8 +2,8 @@
 import json
 import csv
 from datetime import datetime
-from typing import List, Tuple
-# import mysql.connector
+from typing import List, Tuple, Union
+import mysql.connector
 """Extract transform and load tjop data
 
 Assumptions made: data in all files is ordered from first episode to last episode
@@ -62,7 +62,7 @@ def csv_extract() -> Tuple[List[dict], List[dict]]:
     return (colors_used, subject_matter)
 
 
-def subject_to_list(colors_dict: dict) -> list:
+def subject_to_json_list(colors_dict: dict) -> str:
     """ Converts subject booleans to list that matches formatting of other
     JSON lists in database
     """
@@ -73,15 +73,15 @@ def subject_to_list(colors_dict: dict) -> list:
             continue
         if val == '1':
             normal_key = (
-                ''.join(
+                ' '.join(
                     map(
                         lambda k: k[0].upper() + k[1:],
-                        key.lower().split(' ')
-                    )
+                        key.lower().split('_')
+                    ),
                 )
             )
             subjects.append(normal_key)
-    return subjects
+    return json.dumps(subjects)
 
 
 def records_transform(titles_dates,
@@ -109,10 +109,8 @@ def records_transform(titles_dates,
     """
     paintings_arr = []
     episodes_arr = []
-    from_json = (
-        lambda s: json.loads(
-            s.replace("\\n", "").replace("\\r", "").replace("'", '"')
-        )
+    clean_json = (
+        lambda s: s.replace("\\n", "").replace("\\r", "").replace("'", '"')
     )
     for i, colors in enumerate(colors_used):
 
@@ -120,9 +118,9 @@ def records_transform(titles_dates,
             "index": int(colors['painting_index']),
             "name": titles_dates[i][0],
             "img_src": colors['img_src'],
-            "colors": from_json(colors['colors']),
-            "colors_hex": from_json(colors['color_hex']),
-            "subject": subject_to_list(subject_matter[i])
+            "colors": clean_json(colors['colors']),
+            "colors_hex": clean_json(colors['color_hex']),
+            "subject": subject_to_json_list(subject_matter[i])
         })
 
         episodes_arr.append({
@@ -136,21 +134,58 @@ def records_transform(titles_dates,
     return (episodes_arr, paintings_arr)
 
 
+def upload_to_mySQL(
+        db_conn,
+        episodes: List[dict],
+        paintings: List[dict]) -> None:
+    insert_paintings = (
+        "INSERT INTO painting "
+        '(`index`, `name`, `img_src`, `colors`, `colors_hex`, `subject`) '
+        'VALUES (%(index)s, %(name)s, %(img_src)s, %(colors)s, %(colors_hex)s, %(subject)s)'  # noqa
+    )
+
+    insert_episodes = (
+        "INSERT INTO episode "
+        '(`season`, `episode`, `air_date`, `youtube_src`, `painting_index`) '
+        'VALUES (%(season)s, %(episode)s, %(air_date)s, %(youtube_src)s, %(painting_index)s)'  # noqa
+    )
+
+    cursor = db_conn.cursor()
+
+    cursor.executemany(insert_paintings, paintings)
+    cursor.executemany(insert_episodes, episodes)
+
+    db_conn.commit()
+    cursor.close()
+
+
 def main():
+    # extract
     titles_dates = titles_dates_extract()
     colors_used, subject_matter = csv_extract()
+    # transform
     episodes, paintings = records_transform(
         titles_dates,
         subject_matter,
         colors_used
     )
-    print(paintings)
+
+    print(len(episodes), len(paintings))
+
+
+    # load
+    db_conn = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        user='jop_etl_user',
+        password='bobross',
+        db='joy_of_painting'
+    )
+    upload_to_mySQL(db_conn, episodes, paintings)
+    db_conn.close()
 
 
 main()
-# db_conn = mysql.connector.connect(user='jop_etl_user', password='bobross',
-#                                   host='localhost', port=3306,
-#                                   db='joy_of_painting')
 # cursor = db_conn.cursor()
 # query = ("DESCRIBE painting")
 
