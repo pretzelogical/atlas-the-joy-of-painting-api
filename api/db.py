2edf3f7e73f7d877c396365ca68f8f419418d3c8
pretzelogical <yingyangyo266@gmail.com>
2024-04-02 18:39:28 -0500

    json query ALL path

--- api/db.py
+++ api/db.py
@@ -5,6 +5,8 @@ import json
 class TJOPDatabase:
 
     def connect(self, host='localhost', port='5000') -> None:
+        if self.__db:
+            return
         self.__db = mysql.connector.connect(
             host=host,
             port=port,
@@ -13,8 +15,53 @@ class TJOPDatabase:
             password='bobross'
         )
 
+    def build_query_JSON_str(self, query: dict) -> str:
+        """ Build part of query that select JSON values """
+        json_str = ""
+        if not (query.get('colors') or query.get('subject')):
+            return ''
+        if query["match"] == "all":
+            # If strict match then use JSON_CONTAINS and AND conditionals
+            json_str += '( '
 
-    def build_query(self, query: dict) -> str:
+            for col in query.get('colors', []):
+                json_str += (
+                    f"JSON_CONTAINS(painting.colors, '\"{col}\"', '$') AND "
+                )
+
+            for sub in query.get('subject', []):
+                json_str += (
+                    f"JSON_CONTAINS(painting.subject, '\"{sub}\"', '$') AND "
+                )
+            json_str = json_str[:-4]
+            json_str += ') '
+        else:
+            # If loosely match then use JSON_OVERLAPS
+            if query.get('colors'):
+                json_str += (
+                    "( JSON_OVERLAPS(painting.colors, "
+                    f"'{json.dumps(query['colors'])}') "
+                )
+
+            if not query.get('subject'):
+                json_str += ') '
+            else:
+                if query.get('colors'):
+                    json_str += 'AND '
+                else:
+                    json_str += '( '
+                json_str += (
+                    "JSON_OVERLAPS(painting.subject, "
+                    f"'{json.dumps(query['subject'])}') ) "
+                )
+        return json_str
+
+    def build_query_date_str(self, query: dict) -> str:
+        """ Build year and month part of query """
+        return ''
+
+    def build_query_str(self, query: dict) -> str:
+        """ Build query from dict """
         query_str = (
             "SELECT episode.air_date, episode.episode, episode.season, "
             "painting.name, painting.colors, painting.subject "
@@ -22,30 +69,12 @@ class TJOPDatabase:
             "JOIN painting ON episode.painting_index = painting.index "
             "WHERE "
         )
-        condition = ""
-        if query["match"] == "all":
-            condition = 'AND'
-        else:
-            condition = 'OR'
-            # If loosely match then use JSON_OVERLAPS
-            if query.get('colors'):
-                query_str += (
-                    "( JSON_OVERLAPS(painting.colors, "
-                    f"'{json.dumps(query['colors'])}') "
-                )
-
-            if not query.get('subject'):
-                query_str += ') '
-            else:
-                if query.get('colors'):
-                    query_str += 'AND '
-                else:
-                    query_str += '( '
-                query_str += (
-                    "JSON_OVERLAPS(painting.subject, "
-                    f"'{json.dumps(query['subject'])}') ) "
-                )
-
+        json_str = self.build_query_JSON_str(query)
+        query_str += json_str
+        # if json_str != '':
+        #     query_str += 'AND '
+        # date_str = self.build_query_date_str(query)
+        # query_str += date_str
 
         # query_str += "( "
 
--- api/test/test_db.py
+++ api/test/test_db.py
@@ -8,11 +8,31 @@ class TestTJOPDatabase(unittest.TestCase):
         self.db = TJOPDatabase()
 
     def test_build_query_match_all(self):
-        built_query = self.db.build_query({
+        built_query = self.db.build_query_str({
+            "match": "all",
+            "month": "1983-02",
+            "colors": ["Prussian Blue", "Sap Green", "Titanium White"],
+            "subject": ["Mountain", "Grass", "Trees"]
+        })
+
+        print(built_query, "\n")
+
+    def test_build_query_match_some(self):
+        built_query = self.db.build_query_str({
             "match": "some",
             "month": "1983-02",
             "colors": ["Prussian Blue", "Sap Green", "Titanium White"],
             "subject": ["Mountain", "Grass", "Trees"]
         })
 
-        print(built_query)
+        print(built_query, "\n")
+
+    def test_build_query_match_all_month(self):
+        built_query = self.db.build_query_str({
+            "match": "all",
+            "month": "1983-02",
+            # "colors": ["Prussian Blue", "Sap Green", "Titanium White"],
+            # "subject": ["Mountain", "Grass", "Trees"]
+        })
+
+        print(built_query, "\n")
